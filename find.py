55990d704568e1f9eb7aa8caea7002eeeca35bb1
Jingyi Xu <jx15@ualberta.ca>
2023-01-10 13:01:25 -0700

    lab1 files new version

--- find.py
+++ find.py
@@ -1,4 +1,7 @@
-import requests;
 
-print(requests.get("https://www.google.com").text)
-print(requests.__version__)
+import requests
+
+url = "https://raw.githubusercontent.com/xuclaire0234/404Lab1/main/find.py"
+resp = requests.get(url)
+with open("find.py", "r") as f:
+    exec(f.read())
