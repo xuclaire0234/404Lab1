e921a5e1ae31b31766ef3ed84552f3c779cc3acf
Jingyi (Claire) Xu <75157518+xuclaire0234@users.noreply.github.com>
2023-01-12 00:32:16 -0700

    Update find.py

--- find.py
+++ find.py
@@ -1,5 +1,9 @@
-import requests;
-print(requests.get("https://www.google.com").text)
-print(requests.__version__)
+# import requests;
+# print(requests.get("https://www.google.com").text)
+# print(requests.__version__)
 
+import requests
+url = "https://raw.githubusercontent.com/xuclaire0234/404Lab1/main/find.py"
+resp = requests.get(url)
+print(resp.text)
 
